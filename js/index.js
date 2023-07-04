6a5507eed55866ad30c3f5ff474dcbcf35a273c2
Shivamsoni001 <shivam25ic053@satiengg.in>
2023-07-04 06:59:07 +0530

    portfolio website

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const e = require("express");
+
+const p_btns = document.querySelector(".p-btns");
+const p_btn = document.querySelectorAll(".p-btn");
+const p_img_elem =document.querySelectorAll(".img-overlay");
+
